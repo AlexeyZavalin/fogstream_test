d4dea554a023fbb5500be571bbe948b64af492f5
Alexey Zavalin <lehazavalin95@gmail.com>
2019-11-12 22:32:02 +1000

    Добавлена модель сообщения

--- fogstreamtest/mainapp/models.py
+++ fogstreamtest/mainapp/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+class Message(models.Model):
+    STATUSES = [
+        ('SC', 'Successfully'),
+        ('US', 'Unsuccessfully'),
+    ]
+
+    email = models.EmailField(max_length=254)
+    message = models.TextField()
+    status = models.CharField(max_length=2, choices=STATUSES, default='US')
+
+    def __str__(self):
+        return f'{self.email} - {self.status}'
+
+    class Meta:
+        verbose_name = 'Сообщение'
+        verbose_name_plural = 'Сообщения'
