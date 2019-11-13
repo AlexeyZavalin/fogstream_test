5907decd3fc9dbc74a8d6b36e2ee9ea4f2d37b2a
Alexey Zavalin <lehazavalin95@gmail.com>
2019-11-13 22:47:59 +1000

    Созданы формы отправки сообщения, регистрации и входа

--- fogstreamtest/mainapp/urls.py
+++ fogstreamtest/mainapp/urls.py
@@ -4,6 +4,8 @@ import mainapp.views as mainapp
 app_name = 'mainapp'
 
 urlpatterns = [
+    path('', mainapp.index, name='index'),
     path('register', mainapp.UserCreateView.as_view(), name='register_user'),
-    path('login', mainapp.user_login, name='login')
+    path('login', mainapp.user_login, name='login'),
+    path('message', mainapp.MessageCreateView.as_view(), name='send_message')
 ]
--- fogstreamtest/mainapp/views.py
+++ fogstreamtest/mainapp/views.py
@@ -1,19 +1,51 @@
 from django.shortcuts import render, get_object_or_404, HttpResponseRedirect
-from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.forms import AuthenticationForm
+from mainapp.forms import UserRegisterForm
 from django.urls import reverse_lazy
 from django.contrib.auth.models import User
+from mainapp.models import Message
+from mainapp.forms import MessageForm
 from django.urls import reverse
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import user_passes_test
+from django.http import JsonResponse
 
 from django.views.generic.edit import CreateView
 
 from django.utils.decorators import method_decorator
 
 
-class UserCreateView(CreateView):
+class AjaxableResponseMixin:
+    """
+    Mixin to add AJAX support to a form.
+    Must be used with an object-based FormView (e.g. CreateView)
+    """
+
+    def form_invalid(self, form):
+        response = super().form_invalid(form)
+        if self.request.is_ajax():
+            return JsonResponse(form.errors, status=400)
+        else:
+            return response
+
+    def form_valid(self, form):
+        # We make sure to call the parent's form_valid() method because
+        # it might do some processing (in the case of CreateView, it will
+        # call form.save() for example).
+        response = super().form_valid(form)
+        if self.request.is_ajax():
+            data = {
+                'pk': self.object.pk,
+            }
+            return JsonResponse(data)
+        else:
+            return response
+
+
+class UserCreateView(AjaxableResponseMixin, CreateView):
     model = User
     success_url = reverse_lazy('mainapp:login')
-    form_class = UserCreationForm
+    form_class = UserRegisterForm
 
 
 def user_login(request):
@@ -25,7 +57,7 @@ def user_login(request):
             user = authenticate(username=username, password=password)
             if user:
                 login(request, user)
-                return HttpResponseRedirect(reverse('mainapp:index'))
+                return HttpResponseRedirect(reverse('mainapp:send_message'))
     else:
         form = AuthenticationForm()
 
@@ -33,3 +65,15 @@ def user_login(request):
         'form': form,
     }
     return render(request, 'mainapp/login.html', context)
+
+
+def index(request):
+    return HttpResponseRedirect(reverse('mainapp:login'))
+
+
+@method_decorator(user_passes_test(lambda u: u.is_authenticated), name='dispatch')
+class MessageCreateView(AjaxableResponseMixin, CreateView):
+    model = Message
+    form_class = MessageForm
+    success_url = reverse_lazy('mainapp:send_message')
+    template_name = 'mainapp/message.html'
